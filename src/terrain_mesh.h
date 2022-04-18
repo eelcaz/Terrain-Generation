7ca67c3a730660bdab89b65fe4d3e5fe5968260e
Eric Lai <yun26603020@gmail>
2022-04-18 01:09:57 -0400

    comment on setting zoom value

--- src/terrain_mesh.h
+++ src/terrain_mesh.h
@@ -6,6 +6,7 @@ class Terrain {
 private:
     static double fbmNoise(double z, double x, int octaves);
     static const int TERRAIN_AMPLITUDE = 200;
+    /* good zoom values seem to be 256/CHUNK_WIDTH */
     static const int TERRAIN_ZOOM = 16;
 public:;
     static int** generateChunkHeightMap(int z, int x);
