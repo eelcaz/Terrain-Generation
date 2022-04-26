682db175af768afbcd2c62ce394fe25d1a4a14b1
Eric Lai <yun26603020@gmail>
2022-04-25 23:23:29 -0400

    set random gradients in a flat array for passing to GPU

--- src/perlin_noise_3d.h
+++ src/perlin_noise_3d.h
@@ -27,8 +27,9 @@ private:
     static constexpr double M_PI = 3.14159265358979323846;
     Vector3 gradients[tableSize];
 public:
-    PerlinNoise3D(unsigned int seed);
+    double gradientsGPU[tableSize * 3];
     PerlinNoise3D();
+    PerlinNoise3D(unsigned int seed);
     double interpolate(double a, double b, double weight);
     Vector3 randomGradient(int y, int z, int x);
     double dotProduct(int GridY, int GridZ, int GridX, double py, double pz, double px);
