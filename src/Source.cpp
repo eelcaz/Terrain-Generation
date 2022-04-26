d74c8cf0437c3a296fc975de26b34f4f3b9802ce
Eric Lai <yun26603020@gmail>
2022-04-26 18:53:08 -0400

    consistent typing

--- src/Source.cpp
+++ src/Source.cpp
@@ -252,7 +252,7 @@ int main(int argc, char** argv) {
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3*sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
